2ca5887ecb60b7b5409336af99505acfa9ef575e
Eyuel@369 <asfaweyoel@gmail.com>
2026-01-05 23:20:49 +0300

    feat: Add asset_url configuration to app.php and update entrypoint.sh for dynamic URL assignment

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env sh
 set -e
 
+if [ -n "${RENDER_EXTERNAL_URL:-}" ]; then
+    if [ -z "${APP_URL:-}" ]; then
+        export APP_URL="${RENDER_EXTERNAL_URL}"
+    fi
+    if [ -z "${ASSET_URL:-}" ]; then
+        export ASSET_URL="${RENDER_EXTERNAL_URL}"
+    fi
+fi
+
 if [ "${RUN_MIGRATIONS:-true}" = "true" ]; then
     tries="${DB_WAIT_TRIES:-10}"
     delay="${DB_WAIT_DELAY:-3}"
